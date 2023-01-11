3d6a88ce976486bcfef0e71db7b74f54b3481902
amos <amosjshinkle@gmail.com>
2023-01-11 10:03:37 -0500

    revised fetch call with for loops

--- index.js
+++ index.js
@@ -5,25 +5,61 @@ const myRandomNumber = Math.random() * 1000 | 0;
 
 const baseURL = "https://api.artic.edu/api/v1/artworks"
 
+//need to add a button to refresh the page and regenerate this call, to pull in new info to loadImages
+// const refreshButton = // 
+
+let pieceTitles = [];
+let artistNames = [];
+let datesCreated = [];
+let mediumOfArt = [];
+let placesOfOrigin = [];
+let categoryTags = [];
+
 function loadImages () {
     fetch('https://api.artic.edu/api/v1/artworks')
     .then((resp) => resp.json())
     .then((json) => {
-    console.log(json)
-    console.log(json.pagination.current_page)
-    console.log(json.data[0].title)
-    console.log(json.data[0].artist_title)
-    console.log(json.data[0].date_display)
-    console.log(json.data[0].medium_display)
-    console.log(json.data[0].place_of_origin)
-    console.log(json.data[0].category_titles)
+    for (let i = 0; i < json.data.length; ++i) {
+            pieceTitles.push(json.data[i].title);
+        }
+
+    for (let i = 0; i < json.data.length; ++i) {
+        artistNames.push(json.data[i].artist_title)
+    }
+
+    for (let i = 0; i < json.data.length; ++i) {
+        datesCreated.push(json.data[i].date_display)
+    }
+
+    for (let i = 0; i < json.data.length; ++i) {
+        mediumOfArt.push(json.data[i].medium_display)
+    }
+
+    for (let i = 0; i < json.data.length; ++i) {
+        placesOfOrigin.push(json.data[i].place_of_origin)
+    }
+
+    for (let i = 0; i < json.data.length; ++i) {
+        categoryTags.push(json.data[i].category_titles)
+    }
+
+    for (let i = 0; i < json.data.length; ++i) {
+        let element = json.data[i];
+        console.log(json.pagination[i].current_page)
+    }
+
     const imageList = document.getElementById("art-image-list");
     let artImg = document.createElement("li");
     imageList.appendChild(artImg);
     })
   };
 
- 
+ function addInfo () {
+    //This function will take the data from the API call above and add it to image cards
+    
+ }
+
+
 //Going to use pageOptions to display in Dropdown to user
 const pageOptions = Math.random() * 9914 | 0;
 //Need to create the dropdown and add Event Listener
