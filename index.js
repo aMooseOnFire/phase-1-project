1cf6ef1c9cb6d9dd5da3d9874c7bdfcda8cef9ed
amos <amosjshinkle@gmail.com>
2023-01-12 11:42:36 -0500

    reworked DOM manip

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 //document.addEventListener("DOMContentLoaded", loadImages())
 
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("user-greeting").textContent = "Welcome back, Friend";
+});
+
 //use this to get a random page (not ID)
 const myRandomNumber = Math.random() * 1000 | 0;
 
@@ -17,45 +21,15 @@ let mediumOfArt = [];
 let placesOfOrigin = [];
 let categoryTags = [];
 
-//fetch v2 
-
-function loadImageInfoVF () {
-    fetch("https://api.artic.edu/api/v1/artworks")
-    .then((resp) => resp.json())
-    .then(data => {
-        const artworks = data.data;
-
-        artworks.forEach(artwork => {
-            const image = document.createElement("img");
-            const title = document.createElement("p");
-            const artist = document.createElement("p");
-            const container = document.createElement("div");
-
-        image.src = artwork.attributes.primary_image_url;
-
-        title.textContent = artwork.attributes.title;
-        artist.textContent = artwork.attributes.artist_credit;
-
-        container.appendChild(image);
-        container.appendChild(title);
-        container.appendChild(artist);
-
-        document.getElementById("artwork-gallery").appendChild(container);    
-
-        })
-    })
-    .catch(error => console.error(error));
-}
-
-loadImageInfoVF();
-
-// Image load API
-//function loadImageInfo () {
+//image load 
+function loadImageInfo () {
     fetch("https://api.artic.edu/api/v1/artworks")
     .then((resp) => resp.json())
     .then((json) => {
     for (let i = 0; i < json.data.length; ++i) {
             pieceTitles.push(json.data[i].title);
+            let listItem = document.createElement('li');
+            listItem.textContent = `$data.title`
         }
 
     for (let i = 0; i < json.data.length; ++i) {
@@ -82,26 +56,17 @@ loadImageInfoVF();
 
     })
   };
-//loadImageInfo();
+
+loadImageInfo();
 
  //function to grab info from arrays above and put into list
  function createTitleElement () {
     for (let i=0; i <pieceTitles.length; i++){
-        listHTML += "<li>" + pieceTitles[i] + "</li>";
-        //console.log(i)
-        //let ul = document.getElementById("art-image-list");
-        //let li = document.createElement('li');
-        //ul.appendChild(li);
-        //li.innerHTML=li.innerHTML + array[i];
+        let listItem = document.createElement('li');
+            listItem.textContent = `$pieceTitle[i]`
         }
-    }
- //const listHTML = document.getElementById("art-image-list")
- //createTitleElement();
-
- //Create the list of items
- // const imageList = document.getElementById("art-image-list");
-    //let artImg = document.createElement("li");
-    //imageList.appendChild(artImg);
+    };
+createTitleElement();
 
 //Going to use pageOptions to display in Dropdown to user
 const pageOptions = Math.random() * 9914 | 0;
@@ -113,5 +78,12 @@ document.getElementById("select1").addEventListener("change", function() {
 
 //Allow user to search API based on terms
 
-
 //Search the API functionality for terms/tags: https://api.artic.edu/api/v1/artworks/search?q=cats
+
+//Hide the artist name and origin and have user guess
+
+//Input box
+
+//REveal functionality 
+
+//Refresh page to show another piece of art 
